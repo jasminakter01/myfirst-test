894b57027d3bb3e0ee82b5da5fd6f9528c92ff29
jasmin <jasmin.akter283997@gmail.com>
2020-09-19 14:43:15 +0600

    Added new mathod called display

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
     public static void main(String arg[]){
         System.out.println("I AM JAsmin");
+        display("JAsmin")
+    }
+    public static void display(String name)
+    {
+        System.out.println("I AM JAsmin"+name);
+
     }
 }
