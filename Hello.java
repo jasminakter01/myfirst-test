ecfdc994ebd1239ba630c3e348fed2caa7584011
jasmin <jasmin.akter283997@gmail.com>
2020-09-20 14:50:31 +0600

    All mathod remove and message mathod

--- Hello.java
+++ Hello.java
@@ -2,17 +2,9 @@ public class Hello{
     public static void main(String arg[]){
          
         System.out.println("I AM JAsmin");
- 
-        display("GIT")
- 
-        display();
+  
     }
-    public static void display()
-    {
- 
-        System.out.println("I AM MAster");
-   System.out.println("Hello HEllo I am BranchA");
- 
-
-    }
+   public static void message(){
+       System.out.println("Wow! Now We Are able to contribute of open source");
+   }
 }
