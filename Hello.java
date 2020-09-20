84a5bd35779111de07dfdfc82354dd7f74f3006a
jasmin <jasmin.akter283997@gmail.com>
2020-09-20 13:19:13 +0600

    Add Notthing File

--- Hello.java
+++ Hello.java
@@ -1,11 +1,11 @@
 public class Hello{
     public static void main(String arg[]){
         System.out.println("I AM JAsmin");
-        display("JAsmin")
+        display("GIT")
     }
     public static void display(String name)
     {
-        System.out.println("I AM JAsmin"+name);
+        System.out.println("I AM Faria"+name);
 
     }
       public static void squre(int a)
